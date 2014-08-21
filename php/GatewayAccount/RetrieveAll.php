59a2c2e8543447cd054746f887b72eb7452a6866
Kayce Basques <kayce.basques@arrayent.com>
2014-08-21 15:55:10 -0700

    Restructure files and code to conform to other PayFabric examples.

---
+++ php/GatewayAccount/RetrieveAll.php
@@ -0,0 +1,39 @@
+<?php
+
+const DEVICE_ID = "49b8e79d-bc02-9295-fe09-a4112427490c";
+const DEVICE_PASSWORD = "Samsonite";
+
+class GatewayAccount {
+    
+    public function retrieveAll() {
+
+        // Setup the HTTP request.
+        $httpUrl = "https://sandbox.payfabric.com/rest/v1/api/setupid";
+        $httpHeader = Array(
+                "Content-Type: application/json",
+                "authorization: " . DEVICE_ID . "|" . DEVICE_PASSWORD);        
+        $curlOptions = Array(CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_HTTPHEADER => $httpHeader);
+
+        // Execute the HTTP request.
+        $curlHandle = curl_init($httpUrl);
+        curl_setopt_array($curlHandle, $curlOptions);
+        $httpResponseBody = curl_exec($curlHandle);
+        $httpResponseCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+        curl_close($curlHandle);
+
+        if ($httpResponseCode >= 300) {
+            // Handle errors.
+        }    
+
+        // Convert the JSON into a multi-dimensional array.
+        $gatewayAccountsArray = json_decode($httpResponseBody, TRUE);
+
+        // Output the results of the request.
+        var_dump($gatewayAccountsArray);
+
+        return $gatewayAccountsArray;
+        
+    }
+    
+}
---
+++ php/Wallet/Customer.php
@@ -0,0 +1,96 @@
+<?php
+
+require_once "Constants.php";
+require_once "HttpClient.php";
+require_once "Utilities.php";
+
+/* Customer - Handles customer management tasks.
+ * 
+ * Use this class to access a Customer's addresses, credit cards, etc.
+ * Pass the Customer ID of the Customer as the argument constructor. */
+class Customer {
+    private $_id; // Customer ID
+
+    function __construct($id) {
+        $this->_id = $id;
+    }
+
+    /* CARDS */
+    public function createCreditCard($creditCardArray) {
+        /* These arrays contain all of the fields that are required when
+         * creating a new credit card. */
+        $addressFields = Array("Customer", "Line1", "City", "State", "Country",
+                "Zip");
+        $cardholderFields = Array("FirstName", "LastName");
+        $creditCardFields = Array("Tender", "Customer", "Account", "ExpDate",
+                "CardHolder", "Billto");
+
+        // Check that the user's credit card array contains all required fields.
+        foreach ($creditCardFields as $field) {
+            if (!array_key_exists($field, $creditCardArray)) {
+                throw new Exception("missingRequiredCardFieldException");
+            }
+        }
+        foreach ($cardholderFields as $field) {
+            if (!array_key_exists($field, $creditCardArray["CardHolder"])) {
+                throw new Exception("missingRequiredCardholderFieldException");
+            }
+        }
+        foreach ($addressFields as $field) {
+            if (!array_key_exists($field, $creditCardArray["Billto"])) {
+                throw new Exception("missingRequiredAddressFieldException");
+            }
+        }
+
+        // Send the request to PayFabric.
+        $result = HttpClient::post("wallet/create", 
+                Utilities::toJsonFromArray($creditCardArray));
+
+        // Parse PayFabric response and return status to user.
+        if (is_null($result["Result"])) {
+            return FALSE;
+        }
+        return $result["Result"];
+    }
+
+    public function deleteCreditCard($creditCardId) {
+        $result = HttpClient::get("wallet/delete/" . $creditCardId);
+        return $result["Result"] === "True";
+    }
+ 
+    public function getAllCreditCards() {
+        return HttpClient::get("wallet/get/" . $this->_id . "?tender=" . 
+                Constants::TENDER_TYPE_CREDIT_CARD);
+    }
+
+    public function getAllEchecks() {
+        return HttpClient::get("wallet/get/". $this->_id . "?tender=" . 
+                Constants::TENDER_TYPE_ECHECK);
+    }
+
+    public function deleteEcheck($echeckId) {
+        return $this->deleteCreditCard($echeckId);
+    }
+
+    public function getId() {
+        return $this->_id;
+    }
+   
+    public function updateCreditCard($creditCardArray) {
+        if (isset($creditCardArray["Account"])) {
+            unset($creditCardArray["Account"]);
+        }
+        $json = Utilities::toJsonFromArray($creditCardArray);
+        $result = HttpClient::post("wallet/update", $json);
+        return $result["Result"] === "True";
+    }
+
+    /* ADDRESSES */
+    public function getAllAddresses() {
+        return HttpClient::get("addresses/" . $this->_id);
+    }
+
+    public function getAddressById($addressId) {
+        return HttpClient::get("address/" . $addressId);
+    }
+}
