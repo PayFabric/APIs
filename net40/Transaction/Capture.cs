88851fb050f36ec8bf66783b5a886230a9773847
RenaWu <rena.wu@nodus.com>
2015-04-03 11:29:58 +0800

    Update Capture.cs

--- net40/Transaction/Capture.cs
+++ net40/Transaction/Capture.cs
@@ -70,5 +70,94 @@ namespace Samples.Net40
             }
         }
         
+        /// <summary>
+        /// Capture with whole transaction object. in this way, can pass new ship amount to do partial capture
+        /// <param name="originalKey">Orignial book transaction key</param>
+        /// </summary>
+        public void PartialCapture()
+        {
+            try
+            {
+                //  Populate POST String
+                StringBuilder datastring = new StringBuilder();
+                datastring.Append("{");
+                datastring.Append("\"ReferenceKey\":\""+originalKey+"\",");
+                datastring.Append("\"Customer\":\"ARRONFIT0003\",");
+                datastring.Append("\"Currency\":\"USD\",");
+                datastring.Append("\"Amount\":\"10.05\",");
+                datastring.Append("\"Type\":\"Ship\",");
+                datastring.Append("\"SetupId\":\"Paypal\","); // Replace with your gateway account profile name
+                datastring.Append("\"Card\":{");
+                datastring.Append("\"Account\":\"5555555555554444\",");
+                datastring.Append("\"Cvc\":\"1453\",");
+                datastring.Append("\"Tender\":\"CreditCard\",");
+                datastring.Append("\"CardName\":\"MasterCard\",");
+                datastring.Append("\"ExpDate\":\"0117\",");
+                datastring.Append("\"CardHolder\":{");
+                datastring.Append("\"FirstName\":\"Jason\",");
+                 datastring.Append("\"MiddleName\":\"\",");
+                 datastring.Append("\"LastName\":\"Zhao\""); 
+                datastring.Append("},");
+                datastring.Append("\"Billto\":{");
+                datastring.Append("\"Country\":\"US\",");
+                datastring.Append("\"State\":\"CA\",");
+                datastring.Append("\"City\":\"ANAHEIM\",");
+                datastring.Append("\"Zip\":\"92806\",");
+                datastring.Append("\"Line1\":\"2099 S State College Blvd\",");
+                datastring.Append("\"Email\":\"support@payfabric.com\"");
+                datastring.Append("}");
+                datastring.Append("}");
+                datastring.Append("}");
+
+                // POST
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
+                var url = "https://sandbox.payfabric.com/V2/rest/api/transaction/process";
+                HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
+                httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
+                httpWebRequest.ContentType = "application/json; charset=utf-8";
+                httpWebRequest.Headers["authorization"] = new Token().Create();
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentLength = data.Length;
+                Stream stream = httpWebRequest.GetRequestStream();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+                HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                Stream responseStream = httpWebResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream);
+                string result = streamReader.ReadToEnd();
+                streamReader.Close();
+                responseStream.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+
+                // 
+                // Sample repsonse is similar to below
+                //
+                //{
+                //    "AVSAddressResponse": null,
+                //    "AVSZipResponse": null,
+                //    "AuthCode": null,
+                //    "CVV2Response": null,
+                //    "IAVSAddressResponse": null,
+                //    "Message": "Approved",
+                //    "OriginationID": "A70E6C184BA5",
+                //    "RespTrxTag": null,
+                //    "ResultCode": "0",
+                //    "Status": "Approved",
+                //    "TrxDate": "5\/31\/2014 3:17:27 PM",
+                //    "TrxKey": "140531067716"
+                //}
+
+            }
+            catch (WebException e)
+            {
+                //  Handling exception from PayFabric
+            }
+            catch (Exception e)
+            {
+                //  Handling exception
+            }
+
+        }
     }
 }
