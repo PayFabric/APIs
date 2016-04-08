ad71fe5c5ab8624a5361e6a73a36a0fe6171c7ef
ShaunSharples <Shaun.Sharples@nodus.com>
2016-04-08 14:17:25 +1200

    Update Capture.cs

--- Samples/net40/Transaction/Capture.cs
+++ Samples/net40/Transaction/Capture.cs
@@ -29,7 +29,7 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/v2/rest/api/reference/" + preAuthorizedKey + "?trxtype=Ship";
+                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/reference/" + preAuthorizedKey + "?trxtype=Ship";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
                 httpWebRequest.Headers["authorization"] = new Token().Create(); 
@@ -111,7 +111,7 @@ namespace Samples.Net40
 
                 // POST
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
-                var url = "https://sandbox.payfabric.com/V2/rest/api/transaction/process";
+                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/transaction/process";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
