1109ebe6563ac58d4538f856274ec273052c10d8
ShaunSharples <Shaun.Sharples@nodus.com>
2016-04-08 14:17:02 +1200

    Update Create.cs

--- Samples/net40/Transaction/Create.cs
+++ Samples/net40/Transaction/Create.cs
@@ -41,7 +41,7 @@ namespace Samples.Net40
 
                 // POST
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
-                var url = "https://sandbox.payfabric.com/V2/Rest/api/transaction/create";
+                var url = "https://sandbox.payfabric.com/V3/PayFabric/Rest/api/transaction/create";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
                 httpWebRequest.Headers["authorization"] = new Token().Create();
