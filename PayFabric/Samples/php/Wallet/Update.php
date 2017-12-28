95c68411a3d9b4fc8b8a22117de6ff2065700ad5
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 08:20:51 +0800

    Update Update.php

--- PayFabric/Samples/php/Wallet/Update.php
+++ PayFabric/Samples/php/Wallet/Update.php
@@ -30,7 +30,7 @@ class Wallet {
         $json = json_encode($cardArray, TRUE);
 
         // Setup the HTTP request.
-        $httpUrl = "https://sandbox.payfabric.com/rest/v1/api/wallet/update";
+        $httpUrl = "https://sandbox.payfabric.com/payment/api/wallet/update";
         $httpHeader = Array(
                 "Content-Type: application/json",
                 "authorization: " . DEVICE_ID . "|" . DEVICE_PASSWORD);        
