75bbed5ad00f0a4301846d0c07bf84f0ed2b80b9
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 10:07:37 +0800

    Update gateway_account.rb

--- Samples/ruby/gateway_account.rb
+++ Samples/ruby/gateway_account.rb
@@ -28,7 +28,7 @@ module PayFabric
     module Sandbox
       extend self 
       #  ## Retrieve All Gateway Account Profiles
-      #  [Info](https://github.com/PayFabric/APIs/wiki#retrieve-all-gateway-account-profiles)
+      #  [Info](https://github.com/PayFabric/APIs/blob/v2/Sections/PaymentGatewayProfiles.md#retrieve-payment-gateway-profiles)
       #
       #  Example
       #    
@@ -38,15 +38,15 @@ module PayFabric
       #        {"ID" => "123"}, {"ID" => "124"}, {"ID" => "125"}  
       #      ]
       #    
-      #  @return [Array of Gateway Account](https://github.com/PayFabric/APIs/wiki/API-Objects#gateway-account)
+      #  @return [Array of Gateway Account](https://github.com/PayFabric/APIs/blob/v2/Sections/Objects.md#gateway-account-profile)
       #
       def retrieve_all(device_id, password)
-        url = "https://sandbox.payfabric.com/rest/v1/api/setupid"
+        url = "https://sandbox.payfabric.com/v2/rest/api/setupid"
         Share::retrieve_all(url, device_id, password)
       end  
 
       #  ## Retrieve a Gateway Account Profile By Id
-      #  [Info](https://github.com/PayFabric/APIs/wiki#retrieve-a-gateway-account-profile-by-id)
+      #  [Info](https://github.com/PayFabric/APIs/blob/v2/Sections/PaymentGatewayProfiles.md#retrieve-a-payment-gateway-profile)
       #
       #  +id+ - profile id 
       # 
@@ -56,10 +56,10 @@ module PayFabric
       #    hash = PayFabric::GatewayAccount::Sandbox::retrieve(device_id, password, id) 
       #    hash # => {"ID" => "123"} 
       #
-      # @return [Gateway Account](https://github.com/PayFabric/APIs/wiki/API-Objects#gateway-account)
+      # @return [Gateway Account](https://github.com/PayFabric/APIs/blob/v2/Sections/Objects.md#gateway-account-profile)
       #
       def retrieve(device_id, password, id)
-        url = "https://sandbox.payfabric.com/rest/v1/api/setupid/#{id}"
+        url = "https://sandbox.payfabric.com/v2/rest/api/setupid/#{id}"
         Share::retrieve(url, device_id, password)
       end
     end
@@ -68,12 +68,12 @@ module PayFabric
       extend self 
       # see Sandbox::retrieve_all
       def retrieve_all(device_id, password)
-        url = "https://payfabric.com/rest/v1/api/setupid"
+        url = "https://wwwpayfabric.com/v2/rest/api/setupid"
         Share::retrieve_all(url, device_id, password)
       end  
       # see Sandbox::retrieve
       def retrieve(device_id, password, id)
-        url = "https://payfabric.com/rest/v1/api/setupid/#{id}"
+        url = "https://www.payfabric.com/v2/rest/api/setupid/#{id}"
         Share::retrieve(url, device_id, password)
       end
     end
