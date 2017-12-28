a5012b84622c9be1cf39a5f38eba400d2d9e73ef
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 08:04:29 +0800

    Update Process.cs

--- PayFabric/Samples/net40/Transaction/Process.cs
+++ PayFabric/Samples/net40/Transaction/Process.cs
@@ -27,7 +27,8 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/Rest/api/transaction/process/" + transactionKey;
+                var url = "https://sandbox.payfabric.com/payment/api/transaction/process/" + transactionKey;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -202,7 +203,8 @@ namespace Samples.Net40
 
                 #endregion
 
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/Rest/api/transaction/process";
+                var url = "https://sandbox.payfabric.com/payment/api/transaction/process";
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
