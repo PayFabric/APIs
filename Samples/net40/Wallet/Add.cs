7cf54873865841cff891814a82f81187478c88c8
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 09:45:17 +0800

    Update Add.cs

--- Samples/net40/Wallet/Add.cs
+++ Samples/net40/Wallet/Add.cs
@@ -52,6 +52,7 @@ namespace Samples.Net40
                 // POST
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
                 var url = "https://sandbox.payfabric.com/V2/rest/api/wallet/create";
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
