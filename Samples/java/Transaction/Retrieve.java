0227fada4e33f68a372eb028fb175afd618a4529
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 09:28:26 +0800

    Update Retrieve.java

--- Samples/java/Transaction/Retrieve.java
+++ Samples/java/Transaction/Retrieve.java
@@ -26,7 +26,7 @@ public class Retrieve {
 
 		try {
 
-			String url = "https://sandbox.payfabric.com/rest/v1/api/transaction"
+			String url = "https://sandbox.payfabric.com/v2/rest/api/transaction"
 					+ "/" + transactionKey;
 			URL obj = new URL(url);
 			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
@@ -64,7 +64,7 @@ public class Retrieve {
 			// "result" is a Transaction object with json format
 			//
 			// Go to
-			// https://github.com/PayFabric/APIs/wiki/API-Objects#transaction
+			// https://github.com/PayFabric/APIs/blob/v2/Sections/Objects.md#transaction
 			// for details
 			//
 
