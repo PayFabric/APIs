a99660197ce7a0493fc2eb68f8ed2b5e64361597
JingjingHu <jingjing.hu@nodus.com>
2018-05-24 14:11:11 +0800

    Update Retrieve.cs

--- PayFabric/Samples/net40/GatewayAccount/Retrieve.cs
+++ PayFabric/Samples/net40/GatewayAccount/Retrieve.cs
@@ -35,7 +35,10 @@ namespace Samples.Net40
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
-                httpWebRequest.Headers["authorization"] = new Token().Create();
+                
+                // Replace with your own device id and device password
+                httpWebRequest.Headers["authorization"] = "0ad64468-f4bc-0c99-4e31-bd08dd862c43|123456abc";
+                
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
                 Stream responseStream = httpWebResponse.GetResponseStream();
