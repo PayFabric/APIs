214bb81d47bddc679f8953dad9a3c453b06f656a
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:07:21 +0000

    Update UpdateUserProfile.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Accounts/UpdateUserProfile.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Accounts/UpdateUserProfile.cs
@@ -9,8 +9,8 @@ public void CreateUserProfile(string json, string URL, Token token, ref Response
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/API/Accounts.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Accounts.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Accounts.md#update-an-users-profile for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/AccountUser.md#AccountUserPost for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/users/profile");
