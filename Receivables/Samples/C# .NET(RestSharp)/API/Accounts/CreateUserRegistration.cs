0984781f7db6db99b8708c61c5e0cc7000d0aa51
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:03:38 +0000

    Update CreateUserRegistration.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Accounts/CreateUserRegistration.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Accounts/CreateUserRegistration.cs
@@ -9,8 +9,8 @@ public void CreateUsers(string json, string URL, Token token, ref RegistrationRe
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/API/Accounts.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Accounts.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Accounts.md#retrieve-registration-key for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/CustomerUser.md#CustomerUserRegister for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/users/registration");
