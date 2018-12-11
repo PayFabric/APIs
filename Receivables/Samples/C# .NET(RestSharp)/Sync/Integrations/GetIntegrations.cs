b8f35b39711499efb96a91b74367075aa0f82551
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:44:24 +0000

    Update GetIntegrations.cs

--- Receivables/Samples/C# .NET(RestSharp)/Sync/Integrations/GetIntegrations.cs
+++ Receivables/Samples/C# .NET(RestSharp)/Sync/Integrations/GetIntegrations.cs
@@ -8,8 +8,8 @@ public void GetIntegrations(string URL, Token token, ref IntegrationPagingRespon
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/sync/Integrations.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Integrations.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/Sync/Integrations.md#retrieve-integration-documents for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Integration.md#IntegrationPagingResponse for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "sync/API/integrations?filter.pageSize=10&filter.pageIndex=0");
