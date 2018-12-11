0c11d1abe62697d0565302f8c7ad5adf7eecf98e
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:38:48 +0000

    Update CreateToken.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Token/CreateToken.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Token/CreateToken.cs
@@ -9,7 +9,7 @@ public void CreateToken(string username, string password, string URL, ref Token 
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/API/Token.md for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Token.md#payfabric-receivables-api-authentication for more details about request and response.
 	// ------------------------------------------------------	
 	
 	try
