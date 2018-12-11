e5c7efae0a633164456383de5beda9c6df52c216
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:22:45 +0000

    Update GetCurrency.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Currencies/GetCurrency.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Currencies/GetCurrency.cs
@@ -9,8 +9,8 @@ public void GetCurrency(string URL, string currencyCode, Token token, ref Curren
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/API/Currency.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Currency.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Currencies.md#retrieve-a-currency for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Currency.md#CurrencyResponse for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/currencies?code=" + currencyCode);
