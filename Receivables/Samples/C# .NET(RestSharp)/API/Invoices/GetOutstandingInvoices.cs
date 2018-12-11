e4ca08328e7083e18b39a6ba1adce36f567f594a
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:28:32 +0000

    Update GetOutstandingInvoices.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Invoices/GetOutstandingInvoices.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Invoices/GetOutstandingInvoices.cs
@@ -8,8 +8,8 @@ public void GetOutstandingInvoices(string URL, Token token, ref InvoicePagingRes
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/API/Invoices.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Invoices.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Invoices.md#retrieve-outstanding-invoices-by-paging-query-parameters for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Invoice.md#InvoicePagingResponse for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/invoices/outstanding?filter.pageSize=10&filter.pageIndex=0");
