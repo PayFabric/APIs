610fc4804919a3fc0e2876043a93573681a90364
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:14:26 +0000

    Create GetRegistrationKey.cs

---
+++ Receivables/Samples/C# .NET(RestSharp)/API/Accounts/GetRegistrationKey.cs
@@ -0,0 +1,38 @@
+/// <summary>
+/// API Request to verify the user registration key on PayFabric Receivables
+/// </summary>
+/// <param name="URL">URL of the PayFabric Receivables site</param>
+/// <param name="registrationKey">Registration key string to be retrieved</param>
+/// <param name="token">PayFabric Receivables token object</param>
+/// <param name="responses">Returned response object</param>
+public void VerifyAccessCode(string URL, string registrationKey, Token token, ref RegistrationUser user)
+{
+	// Sample request and response
+	// ------------------------------------------------------
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/API/Accounts.md#retrieve-registration-key for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/CustomerUser.md#CustomerUserRegister for more details about the object.
+	// ------------------------------------------------------
+	
+	var client = new RestClient(URL + "API/users/registration/" + registrationKey);
+	var request = new RestRequest(Method.GET);
+	request.AddHeader("content-type", "application/json");
+	request.AddHeader("authorization", "Bearer " + token.access_token);
+	IRestResponse response = client.Execute(request);
+
+	if (response.StatusCode == System.Net.HttpStatusCode.OK)
+	{
+		try
+		{
+			JsonDeserializer deserial = new JsonDeserializer();
+			user = deserial.Deserialize<RegistrationUser>(response);
+		}
+		catch
+		{
+			user = null;
+		}
+	}
+	else
+	{
+		user = null;
+	}
+}
