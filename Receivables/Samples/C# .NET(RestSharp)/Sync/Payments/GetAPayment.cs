65779776a16dca8fd59e152a1d90b63bdae15976
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:46:53 +0000

    Update and rename GetPayment.cs to GetAPayment.cs

--- Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/GetAPayment.cs
+++ Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/GetAPayment.cs
@@ -9,8 +9,8 @@ public void GetPayment(string URL, string paymentId, Token token, ref PaymentRes
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/sync/Payments.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Payment.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/Sync/Payments.md#retrieve-a-payment for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Payment.md#PaymentResponse for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/payments/" + paymentId);
