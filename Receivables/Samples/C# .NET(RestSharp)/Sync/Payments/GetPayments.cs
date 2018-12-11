07b43cc98d64ab6333d99ec5233006e9d7780163
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:45:54 +0000

    Update GetPayments.cs

--- Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/GetPayments.cs
+++ Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/GetPayments.cs
@@ -8,8 +8,8 @@ public void GetPayments(string URL, Token token, ref PaymentPagingResponse payme
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/sync/Payments.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Payment.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/Sync/Payments.md#retrieve-payments-by-query-parameters for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Payment.md#PaymentPagingResponse for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/payments?filter.pageSize=10&filter.pageIndex=0");
