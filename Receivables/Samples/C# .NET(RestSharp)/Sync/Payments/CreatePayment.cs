be4690f0e9d3796b85efffa7f26590d5bec37d4d
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:46:20 +0000

    Update CreatePayment.cs

--- Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/CreatePayment.cs
+++ Receivables/Samples/C# .NET(RestSharp)/Sync/Payments/CreatePayment.cs
@@ -9,8 +9,8 @@ public void CreatePayment(string json, string URL, Token token, ref Response res
 {
 	// Sample request and response
 	// ------------------------------------------------------
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/APIs/sync/Payments.md for more details about request and response.
-	// Go to https://github.com/NodusTechnologies/ePay-Advantage/blob/master/Sections/Cloud%20API%20Guide/Sections/Objects/Payment.md for more details about the object.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/APIs/Sync/Payments.md#create-or-update-a-payment for more details about request and response.
+	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/Payment.md#PaymentPost for more details about the object.
 	// ------------------------------------------------------
 	
 	var client = new RestClient(URL + "API/payments");
