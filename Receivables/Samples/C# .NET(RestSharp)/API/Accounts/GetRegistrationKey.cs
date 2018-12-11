ba0c8f636d2bf1593ef92969c4990c0c14719729
Michael Daigh <32942780+MichaelDaighNodus@users.noreply.github.com>
2018-12-11 18:18:16 +0000

    Update GetRegistrationKey.cs

--- Receivables/Samples/C# .NET(RestSharp)/API/Accounts/GetRegistrationKey.cs
+++ Receivables/Samples/C# .NET(RestSharp)/API/Accounts/GetRegistrationKey.cs
@@ -2,10 +2,10 @@
 /// API Request to verify the user registration key on PayFabric Receivables
 /// </summary>
 /// <param name="URL">URL of the PayFabric Receivables site</param>
-/// <param name="registrationKey">Registration key string to be retrieved</param>
+/// <param name="json">JSON string to be sent</param>
 /// <param name="token">PayFabric Receivables token object</param>
 /// <param name="responses">Returned response object</param>
-public void VerifyAccessCode(string URL, string registrationKey, Token token, ref RegistrationUser user)
+public void GetRegistrationKey(string URL, string json, Token token, ref AccessCodeRegisterResponse responses)
 {
 	// Sample request and response
 	// ------------------------------------------------------
@@ -13,10 +13,11 @@ public void VerifyAccessCode(string URL, string registrationKey, Token token, re
 	// Go to https://github.com/PayFabric/APIs/blob/master/Receivables/Sections/Objects/CustomerUser.md#CustomerUserRegister for more details about the object.
 	// ------------------------------------------------------
 	
-	var client = new RestClient(URL + "API/users/registration/" + registrationKey);
-	var request = new RestRequest(Method.GET);
+	var client = new RestClient(URL + "API/users/registration/");
+	var request = new RestRequest(Method.POST);
 	request.AddHeader("content-type", "application/json");
 	request.AddHeader("authorization", "Bearer " + token.access_token);
+	request.AddParameter("application/json", json, ParameterType.RequestBody);
 	IRestResponse response = client.Execute(request);
 
 	if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -24,11 +25,11 @@ public void VerifyAccessCode(string URL, string registrationKey, Token token, re
 		try
 		{
 			JsonDeserializer deserial = new JsonDeserializer();
-			user = deserial.Deserialize<RegistrationUser>(response);
+			responses = deserial.Deserialize<CustomerUser>(response);
 		}
 		catch
 		{
-			user = null;
+			usresponseser = null;
 		}
 	}
 	else
